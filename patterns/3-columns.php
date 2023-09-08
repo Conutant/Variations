4cc60503d6f53b48cd8ebf6dca512cb4451e0eec
Conutant <tylermoore@gmail.com>
2023-09-08 08:17:12 -0700

    initial commit

---
+++ patterns/3-columns.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Title: Section with image, text, buttons.
+ * Slug: variations/hero-one-column
+ * Categories: featured
+ */
+?>
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":41,"aspectRatio":"3/4","scale":"cover","sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://nowitsonline.com/wp-content/uploads/2023/08/moon-8170668_1920-1-1024x683.jpg" alt="" class="wp-image-41" style="aspect-ratio:3/4;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"left"} -->
+<h2 class="wp-block-heading has-text-align-left">Heading</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>This is one</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button /--></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":41,"aspectRatio":"3/4","scale":"cover","sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://nowitsonline.com/wp-content/uploads/2023/08/moon-8170668_1920-1-1024x683.jpg" alt="" class="wp-image-41" style="aspect-ratio:3/4;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"left"} -->
+<h2 class="wp-block-heading has-text-align-left">Heading</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>This is one</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button /--></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":41,"aspectRatio":"3/4","scale":"cover","sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://nowitsonline.com/wp-content/uploads/2023/08/moon-8170668_1920-1-1024x683.jpg" alt="" class="wp-image-41" style="aspect-ratio:3/4;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"left"} -->
+<h2 class="wp-block-heading has-text-align-left">Heading</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>This is one</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button /--></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
