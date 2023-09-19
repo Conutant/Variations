60117b3859647463790aa5787556f2a4f47e6124
Maksym Marko <markomaksym@gmail.com>
2023-09-19 13:57:09 +0300

    Made template for Archives page. Modified search page. Modified 404 page.

--- patterns/hidden-404.php
+++ patterns/hidden-404.php
@@ -6,24 +6,18 @@
  */
 
 ?>
-<!-- wp:spacer {"height":"var(--wp--preset--spacing--30)"} -->
-<div style="height:var(--wp--preset--spacing--30)" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
-
-<!-- wp:heading {"level":1,"align":"wide"} -->
-<h1 class="alignwide"><?php echo esc_html_x( '404', 'Error code for a webpage that is not found.', 'variations' ); ?></h1>
-<!-- /wp:heading -->
-
-<!-- wp:group {"align":"wide","layout":{"type":"default"},"style":{"spacing":{"margin":{"top":"5px"}}}} -->
-<div class="wp-block-group alignwide" style="margin-top:5px">
-	<!-- wp:paragraph -->
-	<p><?php echo esc_html_x( 'This page could not be found.', 'Message to convey that a webpage could not be found', 'variations' ); ?></p>
-	<!-- /wp:paragraph -->
-
-	<!-- wp:search {"label":"<?php echo esc_html_x( 'Search', 'label', 'variations' ); ?>","placeholder":"<?php echo esc_attr_x( 'Search...', 'placeholder for search field', 'variations' ); ?>","showLabel":false,"width":100,"widthUnit":"%","buttonText":"<?php esc_attr_e( 'Search', 'variations' ); ?>","buttonUseIcon":true,"align":"center"} /-->
-</div>
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|50"},"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"layout":{"type":"constrained","contentSize":"1200px"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:heading {"textAlign":"left","level":1,"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+<h1 class="wp-block-heading has-text-align-left" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><?php printf( esc_html__( '%s', 'variations' ), __( 'Page Not Found', 'variations' ) ); ?></h1>
+<!-- /wp:heading --></div>
 <!-- /wp:group -->
 
-<!-- wp:spacer {"height":"var(--wp--preset--spacing--70)"} -->
-<div style="height:var(--wp--preset--spacing--70)" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"var:preset|spacing|30","right":"var:preset|spacing|30"},"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"},"blockGap":"0"},"dimensions":{"minHeight":""}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);padding-top:0;padding-right:var(--wp--preset--spacing--30);padding-bottom:0;padding-left:var(--wp--preset--spacing--30)"><!-- wp:paragraph {"align":"center","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"0","bottom":"var:preset|spacing|30","left":"0"},"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}},"textColor":"dark"} -->
+<p class="has-text-align-center has-dark-color has-text-color" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:var(--wp--preset--spacing--30);padding-right:0;padding-bottom:var(--wp--preset--spacing--30);padding-left:0"><?php printf( esc_html__( '%s', 'variations' ), __( 'Unfortunately, the page you are looking for no longer exists, or has been moved. Please try searching for your content below.', 'variations' ) ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"blockGap":"0","margin":{"top":"0","bottom":"0"}},"dimensions":{"minHeight":""}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:search {"showLabel":false,"placeholder":"<?php printf( esc_html__( '%s', 'variations' ), __( 'Search', 'variations' ) ); ?>","width":100,"widthUnit":"%","buttonText":"<?php printf( esc_html__( '%s', 'variations' ), __( 'Search', 'variations' ) ); ?>","style":{"border":{"width":"1px"}},"borderColor":"main-dark","fontSize":"xx-small"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
