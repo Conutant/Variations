2c21937d7842e6ffd0683c36ba7b1475ed951c30
Olena Iaroshynska <olenaiaroshynska@gmail.com>
2023-10-09 17:37:19 +0300

    Added 4 sections of the sustainability template.

---
+++ patterns/Sustainability-3-Columns.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Title: Sustainability 3 Columns
+ * Slug: variations/sustainability-3-columns
+ * Description: Add a Sustainability 3 Columns section
+ * Categories: featured, text, columns
+ * Keywords: columns
+ */
+
+?>
+<!-- wp:group {"tagName":"section","align":"full","style":{"spacing":{"padding":{"left":"0rem","right":"0rem","top":"7.5%","bottom":"7.5%"},"margin":{"top":"0px","bottom":"0px"}}},"layout":{"type":"constrained","wideSize":"1920px","contentSize":"1920px"}} -->
+<section class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:7.5%;padding-right:0rem;padding-bottom:7.5%;padding-left:0rem"><!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"right":"0px","left":"0px"},"blockGap":{"top":"0px","left":"0px"}}}} -->
+<div class="wp-block-columns alignwide" style="padding-right:0px;padding-left:0px"><!-- wp:column {"verticalAlignment":"center","width":"20.5%","layout":{"type":"default"}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:20.5%"><!-- wp:image {"align":"center","id":196,"aspectRatio":"3/4","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/services/sustainability-s2-im1_2.jpg" alt="" class="wp-image-196" style="aspect-ratio:3/4;object-fit:cover"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"61%"} -->
+<div class="wp-block-column" style="flex-basis:61%"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":5,"style":{"typography":{"fontStyle":"normal","fontWeight":"300","fontSize":"3.5rem","textTransform":"capitalize","lineHeight":"1.0"},"spacing":{"margin":{"top":"2rem"}}}} -->
+<h5 class="wp-block-heading has-text-align-center" style="margin-top:2rem;font-size:3.5rem;font-style:normal;font-weight:300;line-height:1.0;text-transform:capitalize"><?php printf( esc_html__( '%s', 'variations' ), __( 'We are Creating', 'variations' ) ); ?></h5>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontSize":"7.5rem","lineHeight":"1.0","fontStyle":"normal","fontWeight":"500"},"spacing":{"margin":{"bottom":"15px"}}}} -->
+<h3 class="wp-block-heading has-text-align-center" style="margin-bottom:15px;font-size:7.5rem;font-style:normal;font-weight:500;line-height:1.0"><?php printf( esc_html__( '%s', 'variations' ), __( 'Sustainability', 'variations' ) ); ?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"1rem","left":"1rem"}}},"layout":{"type":"constrained","contentSize":"721px"}} -->
+<div class="wp-block-group" style="padding-right:1rem;padding-left:1rem"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"1.25rem"}}} -->
+<p class="has-text-align-center" style="font-size:1.25rem"><?php printf( esc_html__( '%s', 'variations' ), __( 'Founded in 1921, Sackett Woodworking is a fourth-generation family business dedicated to the art and craft of fine carpentry. From grand homes to one of a kind heirlooms, we approach every project as a unique opportunity to apple classic woodworking techniques to modern aesthetics.', 'variations' ) ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons {"style":{"spacing":{"margin":{"top":"2rem","bottom":"2rem"}}}} -->
+<div class="wp-block-buttons" style="margin-top:2rem;margin-bottom:2rem"><!-- wp:button {"style":{"border":{"color":"#504f43","width":"1px","radius":"0px"},"typography":{"textTransform":"uppercase","fontSize":"1.11rem"},"spacing":{"padding":{"left":"4.5rem","right":"4.5rem","top":"1rem","bottom":"1rem"}}},"className":"is-style-outline"} -->
+<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:1.11rem;text-transform:uppercase"><a class="wp-block-button__link has-border-color wp-element-button" style="border-color:#504f43;border-width:1px;border-radius:0px;padding-top:1rem;padding-right:4.5rem;padding-bottom:1rem;padding-left:4.5rem"><?php printf( esc_html__( '%s', 'variations' ), __( 'Discover', 'variations' ) ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"20.5%","layout":{"type":"default"}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:20.5%"><!-- wp:image {"align":"center","id":197,"aspectRatio":"3/4","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/services/sustainability-s2-im2_2.jpg" alt="" class="wp-image-197" style="aspect-ratio:3/4;object-fit:cover"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></section>
+<!-- /wp:group -->
---
+++ patterns/Sustainability-Home.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * Title: Sustainability Home Page
+ * Slug: variations/sustainability-homepage
+ * Description: Add a Home Page template
+ * Categories: homepage
+ * Keywords: home, home page, homepage
+ */
+
+?>
+<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/banners/sustainability-hero.jpg","id":185,"dimRatio":0,"overlayColor":"contrast","minHeightUnit":"vh","contentPosition":"center center","align":"full","layout":{"type":"constrained","contentSize":"px"}} -->
+<div class="wp-block-cover alignfull"><span aria-hidden="true" class="wp-block-cover__background has-contrast-background-color has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-185" alt="" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/banners/sustainability-hero.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:spacer {"height":"72vh"} -->
+<div style="height:72vh" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"left","style":{"typography":{"fontStyle":"normal","fontWeight":"400","fontSize":"5.9rem","lineHeight":"1.1"},"spacing":{"margin":{"bottom":"1.1rem"}}},"textColor":"base"} -->
+<h2 class="wp-block-heading has-text-align-left has-base-color has-text-color" style="margin-bottom:1.1rem;font-size:5.9rem;font-style:normal;font-weight:400;line-height:1.1">
+<?php printf( esc_html__( '%s', 'variations' ), __( 'Dare To Be Innovative<br>Care About Sustainability', 'variations' ) ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"left","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}},"typography":{"fontStyle":"normal","fontWeight":"400","fontSize":"1.5rem"}}} -->
+<p class="has-text-align-left" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;font-size:1.5rem;font-style:normal;font-weight:400"><?php printf( esc_html__( '%s', 'variations' ), __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do', 'variations' ) ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"},"style":{"spacing":{"blockGap":"0","margin":{"top":"2.2rem","bottom":"0rem"}}}} -->
+<div class="wp-block-buttons" style="margin-top:2.2rem;margin-bottom:0rem"><!-- wp:button {"textColor":"base","style":{"spacing":{"padding":{"left":"5.4rem","right":"5.4rem","top":"1rem","bottom":"1rem"}},"color":{"background":"#ed6907"},"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"1.11rem"},"border":{"radius":"0px","color":"#ed6907","width":"1px"}},"className":"is-style-fill"} -->
+<div class="wp-block-button has-custom-font-size is-style-fill" style="font-size:1.11rem;font-style:normal;font-weight:500"><a class="wp-block-button__link has-base-color has-text-color has-background has-border-color wp-element-button" style="border-color:#ed6907;border-width:1px;border-radius:0px;background-color:#ed6907;padding-top:1rem;padding-right:5.4rem;padding-bottom:1rem;padding-left:5.4rem"><?php printf( esc_html__( '%s', 'variations' ), __( 'LEARN MORE', 'variations' ) ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:spacer {"height":"122px"} -->
+<div style="height:122px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:group {"tagName":"section","align":"full","style":{"spacing":{"padding":{"left":"0rem","right":"0rem","top":"7.5%","bottom":"7.5%"},"margin":{"top":"0px","bottom":"0px"}}},"layout":{"type":"constrained","wideSize":"1920px","contentSize":"1920px"}} -->
+<section class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:7.5%;padding-right:0rem;padding-bottom:7.5%;padding-left:0rem"><!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"right":"0px","left":"0px"},"blockGap":{"top":"0px","left":"0px"}}}} -->
+<div class="wp-block-columns alignwide" style="padding-right:0px;padding-left:0px"><!-- wp:column {"verticalAlignment":"center","width":"20.5%","layout":{"type":"default"}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:20.5%"><!-- wp:image {"align":"center","id":196,"aspectRatio":"3/4","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/services/sustainability-s2-im1_2.jpg" alt="" class="wp-image-196" style="aspect-ratio:3/4;object-fit:cover"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"61%"} -->
+<div class="wp-block-column" style="flex-basis:61%"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":5,"style":{"typography":{"fontStyle":"normal","fontWeight":"300","fontSize":"3.5rem","textTransform":"capitalize","lineHeight":"1.0"},"spacing":{"margin":{"top":"2rem"}}}} -->
+<h5 class="wp-block-heading has-text-align-center" style="margin-top:2rem;font-size:3.5rem;font-style:normal;font-weight:300;line-height:1.0;text-transform:capitalize"><?php printf( esc_html__( '%s', 'variations' ), __( 'We are Creating', 'variations' ) ); ?></h5>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontSize":"7.5rem","lineHeight":"1.0","fontStyle":"normal","fontWeight":"500"},"spacing":{"margin":{"bottom":"15px"}}}} -->
+<h3 class="wp-block-heading has-text-align-center" style="margin-bottom:15px;font-size:7.5rem;font-style:normal;font-weight:500;line-height:1.0"><?php printf( esc_html__( '%s', 'variations' ), __( 'Sustainability', 'variations' ) ); ?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"1rem","left":"1rem"}}},"layout":{"type":"constrained","contentSize":"721px"}} -->
+<div class="wp-block-group" style="padding-right:1rem;padding-left:1rem"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"1.25rem"}}} -->
+<p class="has-text-align-center" style="font-size:1.25rem"><?php printf( esc_html__( '%s', 'variations' ), __( 'Founded in 1921, Sackett Woodworking is a fourth-generation family business dedicated to the art and craft of fine carpentry. From grand homes to one of a kind heirlooms, we approach every project as a unique opportunity to apple classic woodworking techniques to modern aesthetics.', 'variations' ) ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons {"style":{"spacing":{"margin":{"top":"2rem","bottom":"2rem"}}}} -->
+<div class="wp-block-buttons" style="margin-top:2rem;margin-bottom:2rem"><!-- wp:button {"style":{"border":{"color":"#504f43","width":"1px","radius":"0px"},"typography":{"textTransform":"uppercase","fontSize":"1.11rem"},"spacing":{"padding":{"left":"4.5rem","right":"4.5rem","top":"1rem","bottom":"1rem"}}},"className":"is-style-outline"} -->
+<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:1.11rem;text-transform:uppercase"><a class="wp-block-button__link has-border-color wp-element-button" style="border-color:#504f43;border-width:1px;border-radius:0px;padding-top:1rem;padding-right:4.5rem;padding-bottom:1rem;padding-left:4.5rem"><?php printf( esc_html__( '%s', 'variations' ), __( 'Discover', 'variations' ) ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"20.5%","layout":{"type":"default"}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:20.5%"><!-- wp:image {"align":"center","id":197,"aspectRatio":"3/4","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/services/sustainability-s2-im2_2.jpg" alt="" class="wp-image-197" style="aspect-ratio:3/4;object-fit:cover"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></section>
+<!-- /wp:group -->
+
+<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/banners/sustainability-s3-cover.jpg","id":216,"dimRatio":0,"overlayColor":"contrast","minHeight":73,"minHeightUnit":"vh","contentPosition":"center center","align":"full","style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}},"layout":{"type":"constrained","contentSize":"px"}} -->
+<div class="wp-block-cover alignfull" style="margin-top:0px;margin-bottom:0px;min-height:73vh"><span aria-hidden="true" class="wp-block-cover__background has-contrast-background-color has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-216" alt="" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/banners/sustainability-s3-cover.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:group {"tagName":"section","style":{"spacing":{"padding":{"top":"6.5%","bottom":"6.5%"}}},"layout":{"type":"constrained"}} -->
+<section class="wp-block-group" style="padding-top:6.5%;padding-bottom:6.5%"><!-- wp:group {"style":{"spacing":{"padding":{"bottom":"10%"}}},"layout":{"type":"constrained","contentSize":"1170px"}} -->
+<div class="wp-block-group" style="padding-bottom:10%"><!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"5.5rem","lineHeight":"1.0"}}} -->
+<h3 class="wp-block-heading has-text-align-center" style="font-size:5.5rem;font-style:normal;font-weight:500;line-height:1.0"><?php printf( esc_html__( '%s', 'variations' ), __( 'Contribute to a more sustainable world with every purchase', 'variations' ) ); ?></h3>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:columns {"style":{"spacing":{"blockGap":{"left":"90px"}}}} -->
+<div class="wp-block-columns"><!-- wp:column {"width":"18%","layout":{"type":"default"}} -->
+<div class="wp-block-column" style="flex-basis:18%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"32%","style":{"spacing":{"padding":{"bottom":"4rem"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-column" style="padding-bottom:4rem;flex-basis:32%"><!-- wp:group {"style":{"spacing":{"blockGap":"4rem"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":236,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/services/icon-sea-waves.png" alt="" class="wp-image-236"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":6,"style":{"typography":{"textTransform":"capitalize","fontSize":"2rem","fontStyle":"normal","fontWeight":"500","lineHeight":"1.2"}}} -->
+<h6 class="wp-block-heading" style="font-size:2rem;font-style:normal;font-weight:500;line-height:1.2;text-transform:capitalize"><?php printf( esc_html__( '%s', 'variations' ), __( 'Water', 'variations' ) ); ?></h6>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php printf( esc_html__( '%s', 'variations' ), __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. sed do Pellentesque nec lobortis dui.', 'variations' ) ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":234,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/services/icon-flash.png" alt="" class="wp-image-234"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":6,"style":{"typography":{"textTransform":"capitalize","fontSize":"2rem","fontStyle":"normal","fontWeight":"500","lineHeight":"1.2"}}} -->
+<h6 class="wp-block-heading" style="font-size:2rem;font-style:normal;font-weight:500;line-height:1.2;text-transform:capitalize"><?php printf( esc_html__( '%s', 'variations' ), __( 'Energy', 'variations' ) ); ?></h6>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php printf( esc_html__( '%s', 'variations' ), __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. sed do Pellentesque nec lobortis dui.', 'variations' ) ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"32%","style":{"spacing":{"padding":{"bottom":"4rem"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-column" style="padding-bottom:4rem;flex-basis:32%"><!-- wp:group {"style":{"spacing":{"blockGap":"4rem"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":235,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/services/icon-recycle.png" alt="" class="wp-image-235"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":6,"style":{"typography":{"textTransform":"capitalize","fontSize":"2rem","fontStyle":"normal","fontWeight":"500","lineHeight":"1.2"}}} -->
+<h6 class="wp-block-heading" style="font-size:2rem;font-style:normal;font-weight:500;line-height:1.2;text-transform:capitalize"><?php printf( esc_html__( '%s', 'variations' ), __( 'Waste', 'variations' ) ); ?></h6>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php printf( esc_html__( '%s', 'variations' ), __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. sed do Pellentesque nec lobortis dui.', 'variations' ) ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":231,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/services/icon-cloud.png" alt="" class="wp-image-231"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":6,"style":{"typography":{"textTransform":"capitalize","fontSize":"2rem","fontStyle":"normal","fontWeight":"500","lineHeight":"1.2"}}} -->
+<h6 class="wp-block-heading" style="font-size:2rem;font-style:normal;font-weight:500;line-height:1.2;text-transform:capitalize"><?php printf( esc_html__( '%s', 'variations' ), __( 'Pollution', 'variations' ) ); ?></h6>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php printf( esc_html__( '%s', 'variations' ), __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. sed do Pellentesque nec lobortis dui.', 'variations' ) ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"18%","layout":{"type":"default"}} -->
+<div class="wp-block-column" style="flex-basis:18%"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></section>
+<!-- /wp:group -->
