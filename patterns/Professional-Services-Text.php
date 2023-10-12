2340c6b95b2ffc24d59653d35844f229c7736c86
Olena Iaroshynska <olenaiaroshynska@gmail.com>
2023-10-12 17:48:52 +0300

    Added patterns of header,  footer, first section of Professional Services template. Added template of Home page.

---
+++ patterns/Professional-Services-Text.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Title: Professional Services Text
+ * Slug: variations/professional-services-text
+ * Description: Add a Professional Services text section
+ * Categories: text
+ * Keywords: text, services
+ */
+
+?>
+<!-- wp:group {"tagName":"section","align":"full","style":{"spacing":{"padding":{"left":"1rem","right":"1rem","bottom":"4rem","top":"0rem"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"0px"}},"layout":{"type":"constrained","wideSize":"825px","contentSize":"760px"}} -->
+<section class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:0rem;padding-right:1rem;padding-bottom:4rem;padding-left:1rem"><!-- wp:group {"style":{"spacing":{"blockGap":"2rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontSize":"3.75rem","lineHeight":"1.2","fontStyle":"normal","fontWeight":"600"},"spacing":{"margin":{"bottom":"0rem"}},"color":{"text":"#1f1f1f"}}} -->
+<h3 class="wp-block-heading has-text-align-center has-text-color" style="color:#1f1f1f;margin-bottom:0rem;font-size:3.75rem;font-style:normal;font-weight:600;line-height:1.2"><?php printf( esc_html__( '%s', 'variations' ), __( 'We turn information into actionable insights.', 'variations' ) ); ?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:group {"style":{"typography":{"fontSize":"1.25rem","fontStyle":"normal","fontWeight":"400","lineHeight":"1.5"}},"layout":{"type":"constrained","contentSize":""}} -->
+<div class="wp-block-group" style="font-size:1.25rem;font-style:normal;font-weight:400;line-height:1.5"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"1.25rem","fontStyle":"normal","fontWeight":"400","lineHeight":"1.5"},"spacing":{"margin":{"top":"0px","bottom":"0px"}}}} -->
+<p class="has-text-align-center" style="margin-top:0px;margin-bottom:0px;font-size:1.25rem;font-style:normal;font-weight:400;line-height:1.5"><?php printf( esc_html__( '%s', 'variations' ), __( 'We know how to anticipate, collaborate, and innovate, and create opportunity from even the unforeseen obstacle. You can count on Success to deliver.', 'variations' ) ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"border":{"radius":"0px","width":"0px","style":"none"},"typography":{"textTransform":"capitalize","fontSize":"1.25rem","fontStyle":"normal","fontWeight":"400","lineHeight":"1.5"},"spacing":{"padding":{"left":"2.62rem","right":"2.62rem","top":"1.25rem","bottom":"1.25rem"}},"color":{"text":"#1f1f1f","background":"#ece4dd"}},"className":"is-style-outline"} -->
+<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:1.25rem;font-style:normal;font-weight:400;line-height:1.5;text-transform:capitalize"><a class="wp-block-button__link has-text-color has-background wp-element-button" style="border-style:none;border-width:0px;border-radius:0px;color:#1f1f1f;background-color:#ece4dd;padding-top:1.25rem;padding-right:2.62rem;padding-bottom:1.25rem;padding-left:2.62rem"><?php printf( esc_html__( '%s', 'variations' ), __( 'Learn More', 'variations' ) ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></section>
+<!-- /wp:group -->
