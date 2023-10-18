fc652d6a7cbe7ed2224029c537c036c8ebc08975
Maksym Marko <markomaksym@gmail.com>
2023-10-18 09:38:23 +0300

    Header changes.

--- patterns/Professional-Services-Header.php
+++ patterns/Professional-Services-Header.php
@@ -10,8 +10,8 @@
 
 ?>
 <!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"},"spacing":{"padding":{"left":"1rem","right":"1rem"},"margin":{"top":"0","bottom":"0"}},"color":{"text":"#1f1f1f"},"typography":{"fontSize":"2.6rem","fontStyle":"normal","fontWeight":"400","lineHeight":"1.5"}},"layout":{"type":"constrained","contentSize":"1200px"}} -->
-<div class="wp-block-group has-text-color" style="color:#1f1f1f;margin-top:0;margin-bottom:0;padding-right:1rem;padding-left:1rem;font-size:2.6rem;font-style:normal;font-weight:400;line-height:1.5"><!-- wp:columns {"style":{"spacing":{"padding":{"top":"2rem","bottom":"2rem","left":"3.25rem","right":"3.25rem"},"blockGap":{"top":"1rem","left":"1.62rem"}}}} -->
-<div class="wp-block-columns" style="padding-top:2rem;padding-right:3.25rem;padding-bottom:2rem;padding-left:3.25rem"><!-- wp:column {"verticalAlignment":"center","width":"85%","style":{"spacing":{"padding":{"top":"0px","bottom":"0px","left":"0px","right":"0px"}}}} -->
+<div class="wp-block-group has-text-color" style="color:#1f1f1f;margin-top:0;margin-bottom:0;padding-right:1rem;padding-left:1rem;font-size:2.6rem;font-style:normal;font-weight:400;line-height:1.5"><!-- wp:columns {"isStackedOnMobile":false,"style":{"spacing":{"padding":{"top":"2rem","bottom":"2rem","left":"0rem","right":"0rem"},"blockGap":{"top":"1rem","left":"1.62rem"}}}} -->
+<div class="wp-block-columns is-not-stacked-on-mobile" style="padding-top:2rem;padding-right:0rem;padding-bottom:2rem;padding-left:0rem"><!-- wp:column {"verticalAlignment":"center","width":"85%","style":{"spacing":{"padding":{"top":"0px","bottom":"0px","left":"0px","right":"0px"}}}} -->
 <div class="wp-block-column is-vertically-aligned-center" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px;flex-basis:85%"><!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group"><!-- wp:site-logo {"width":74,"shouldSyncIcon":false,"style":{"layout":{"selfStretch":"fit","flexSize":null}}} /-->
 
@@ -19,13 +19,15 @@
 <!-- /wp:group --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"width":"180px","style":{"spacing":{"padding":{"top":"0px","bottom":"0px","left":"0px","right":"0px"}}},"layout":{"type":"constrained","justifyContent":"center","contentSize":""}} -->
-<div class="wp-block-column" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px;flex-basis:180px"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<!-- wp:column {"width":"260px","style":{"spacing":{"padding":{"top":"0px","bottom":"0px","left":"0px","right":"0px"}}},"layout":{"type":"constrained","justifyContent":"center","contentSize":""}} -->
+<div class="wp-block-column" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px;flex-basis:260px"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"constrained","contentSize":"300px"}} -->
 <div class="wp-block-group"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right","flexWrap":"wrap","orientation":"horizontal"},"style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"margin":{"top":"0.2rem"}}}} -->
 <div class="wp-block-buttons" style="margin-top:0.2rem"><!-- wp:button {"style":{"border":{"radius":"0px","color":"#cccccc","width":"1px"},"typography":{"textTransform":"none","fontSize":"1.1rem","lineHeight":"1.5","fontStyle":"normal","fontWeight":"600"},"spacing":{"padding":{"left":"2.2rem","right":"2.2rem","top":"0.8rem","bottom":"0.8rem"}},"color":{"text":"#1f1f1f"}},"className":"is-style-outline"} -->
-<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:1.1rem;font-style:normal;font-weight:600;line-height:1.5;text-transform:none"><a class="wp-block-button__link has-text-color has-border-color wp-element-button" style="border-color:#cccccc;border-width:1px;border-radius:0px;color:#1f1f1f;padding-top:0.8rem;padding-right:2.2rem;padding-bottom:0.8rem;padding-left:2.2rem">Contact Us</a></div>
+<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:1.1rem;font-style:normal;font-weight:600;line-height:1.5;text-transform:none"><a class="wp-block-button__link has-text-color has-border-color wp-element-button" style="border-color:#cccccc;border-width:1px;border-radius:0px;color:#1f1f1f;padding-top:0.8rem;padding-right:2.2rem;padding-bottom:0.8rem;padding-left:2.2rem"><?php printf( esc_html__( '%s', 'variations' ), __( 'Contact Us', 'variations' ) ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
