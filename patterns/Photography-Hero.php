1109dd1a0ec4ec981d996defeb9321494b722c8f
Maksym Marko <markomaksym@gmail.com>
2023-10-19 09:13:41 +0300

    Photography template started.

---
+++ patterns/Photography-Hero.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Title: Photography Hero
+ * Slug: variations/photography-hero
+ * Description: Add a hero section
+ * Categories: featured, banner
+ * Keywords: hero, hero section
+ */
+
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"3rem"}}},"layout":{"type":"constrained","contentSize":"1200px"}} -->
+<div class="wp-block-group alignfull" style="padding-top:3rem"><!-- wp:group {"style":{"spacing":{"blockGap":"0.9rem"}},"layout":{"type":"constrained","contentSize":"1100px"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":4,"style":{"typography":{"fontSize":"1.2rem","lineHeight":"1.2","fontStyle":"normal","fontWeight":"600"},"spacing":{"margin":{"bottom":"0rem"}},"color":{"text":"#1f1f1f"}}} -->
+<h4 class="wp-block-heading has-text-align-center has-text-color" style="color:#1f1f1f;margin-bottom:0rem;font-size:1.2rem;font-style:normal;font-weight:600;line-height:1.2">SANDRA STEPHENSON</h4>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontSize":"4.4rem","lineHeight":"1.2","fontStyle":"normal","fontWeight":"600"},"spacing":{"margin":{"bottom":"0rem"}},"color":{"text":"#1f1f1f"}}} -->
+<h3 class="wp-block-heading has-text-align-center has-text-color" style="color:#1f1f1f;margin-bottom:0rem;font-size:4.4rem;font-style:normal;font-weight:600;line-height:1.2">Speak to the world through photos</h3>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"5rem"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignfull" style="margin-top:5rem"><!-- wp:image {"id":367,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/photography/image-01.jpg" alt="" class="wp-image-367"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":368,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/photography/image-02.jpg" alt="" class="wp-image-368"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":369,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/photography/image-03.jpg" alt="" class="wp-image-369"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":370,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/photography/image-04.jpg" alt="" class="wp-image-370"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ patterns/Photography-Home.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: Photography Home Page
+ * Slug: variations/photography-homepage
+ * Description: Add a Home Page template
+ * Categories: homepage
+ * Keywords: home, home page, homepage, photography
+ */
+
+?>
+<!-- wp:pattern {"slug":"variations/photography-hero"} /-->
