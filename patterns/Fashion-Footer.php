202cadba2908c39204215ddc9155fe54862393d1
Olena Iaroshynska <olenaiaroshynska@gmail.com>
2023-10-19 19:48:23 +0300

    Finished  Fashion template.

--- patterns/Fashion-Footer.php
+++ patterns/Fashion-Footer.php
@@ -2,68 +2,85 @@
 /**
  * Title: Fashion Footer
  * Slug: variations/fashion-footer
- * Description: Add a Footer With Logo, Columns and Contacts
+ * Description: Add a Footer With Logo, Columns and Copyright
  * Categories: footer, column
  * Keywords: footer
  * Block Types: core/template-part/footer
  */
 
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"5.03rem","left":"1rem","right":"1rem","top":"4.56rem"}},"color":{"background":"#1f1f1f","text":"#fffffd"},"typography":{"fontStyle":"normal","fontWeight":"400","fontSize":"1.25rem","lineHeight":"1.5"}},"layout":{"type":"constrained","contentSize":"1750px"}} -->
-<div class="wp-block-group has-text-color has-background" style="color:#fffffd;background-color:#1f1f1f;padding-top:4.56rem;padding-right:1rem;padding-bottom:5.03rem;padding-left:1rem;font-size:1.25rem;font-style:normal;font-weight:400;line-height:1.5"><!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":{"top":"3.2rem","left":"3.2rem"},"padding":{"right":"4rem","left":"4rem"}}}} -->
-<div class="wp-block-columns alignwide" style="margin-top:0;margin-bottom:0;padding-right:4rem;padding-left:4rem"><!-- wp:column {"verticalAlignment":"top","width":"70%","layout":{"type":"default"}} -->
-<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:70%"><!-- wp:group {"layout":{"type":"constrained","contentSize":"26rem","justifyContent":"left"}} -->
-<div class="wp-block-group"><!-- wp:image {"id":479,"width":"96px","height":"undefinedpx","sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/logos/servo-logo-footer-sm.png" alt="" class="wp-image-479" style="width:96px;height:undefinedpx"/></figure>
+<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"3.03rem","left":"1rem","right":"1rem","top":"3.56rem"}},"typography":{"fontStyle":"normal","fontWeight":"400","fontSize":"1.25rem","lineHeight":"1.5"},"color":{"text":"#525060"}},"layout":{"type":"constrained","contentSize":"800px"}} -->
+<div class="wp-block-group has-text-color" style="color:#525060;padding-top:3.56rem;padding-right:1rem;padding-bottom:3.03rem;padding-left:1rem;font-size:1.25rem;font-style:normal;font-weight:400;line-height:1.5"><!-- wp:image {"align":"center","id":576,"width":"170px","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/fashion/fashion-logo.png" alt="" class="wp-image-576" style="width:170px"/></figure>
 <!-- /wp:image -->
 
-<!-- wp:paragraph {"style":{"color":{"text":"#fffffd"},"typography":{"fontSize":"1.13rem","fontStyle":"normal","fontWeight":"400","lineHeight":"1.5"},"spacing":{"margin":{"bottom":"2.77rem","top":"1.7rem"}}}} -->
-<p class="has-text-color" style="color:#fffffd;margin-top:1.7rem;margin-bottom:2.77rem;font-size:1.13rem;font-style:normal;font-weight:400;line-height:1.5"><?php printf( esc_html__( '%s', 'variations' ), __( 'This is a great space to write long text about your company and your services.', 'variations' ) ); ?></p>
+<!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"blockGap":{"left":"1rem"},"margin":{"top":"2rem","bottom":"3rem"},"padding":{"left":"5%"}}}} -->
+<div class="wp-block-columns are-vertically-aligned-center" style="margin-top:2rem;margin-bottom:3rem;padding-left:5%"><!-- wp:column {"verticalAlignment":"center","width":"","layout":{"type":"constrained"}} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"layout":{"type":"constrained","justifyContent":"center","contentSize":""}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontSize":"0.9rem","lineHeight":"1.2","fontStyle":"normal","fontWeight":"400"},"color":{"text":"#525060"},"elements":{"link":{"color":{"text":"#525060"}}}}} -->
+<p class="has-text-color has-link-color" style="color:#525060;font-size:0.9rem;font-style:normal;font-weight:400;line-height:1.2"><a href="#"><?php printf( esc_html__( '%s', 'variations' ), __( 'Home', 'variations' ) ); ?></a></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:social-links {"customIconColor":"#fffffd","iconColorValue":"#fffffd","openInNewTab":true,"className":"is-style-logos-only"} -->
-<ul class="wp-block-social-links has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"#","service":"facebook"} /-->
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"0.9rem","lineHeight":"1.2","fontStyle":"normal","fontWeight":"400"},"color":{"text":"#525060"},"elements":{"link":{"color":{"text":"#525060"}}}}} -->
+<p class="has-text-color has-link-color" style="color:#525060;font-size:0.9rem;font-style:normal;font-weight:400;line-height:1.2"><a href="#"><?php printf( esc_html__( '%s', 'variations' ), __( 'Shop', 'variations' ) ); ?></a></p>
+<!-- /wp:paragraph -->
 
-<!-- wp:social-link {"url":"#","service":"pinterest"} /-->
-
-<!-- wp:social-link {"url":"#","service":"twitter"} /-->
-
-<!-- wp:social-link {"url":"#","service":"instagram"} /--></ul>
-<!-- /wp:social-links --></div>
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"0.9rem","lineHeight":"1.2","fontStyle":"normal","fontWeight":"400"},"color":{"text":"#525060"},"elements":{"link":{"color":{"text":"#525060"}}}}} -->
+<p class="has-text-color has-link-color" style="color:#525060;font-size:0.9rem;font-style:normal;font-weight:400;line-height:1.2"><a href="#"><?php printf( esc_html__( '%s', 'variations' ), __( 'Blog', 'variations' ) ); ?></a></p>
+<!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"verticalAlignment":"top","width":"140px","style":{"spacing":{"padding":{"right":"0","left":"0"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-column is-vertically-aligned-top" style="padding-right:0;padding-left:0;flex-basis:140px"><!-- wp:heading {"level":6,"style":{"color":{"text":"#fffffd"},"typography":{"fontSize":"1.13rem","fontStyle":"normal","fontWeight":"600","lineHeight":"1.3","textTransform":"capitalize"}}} -->
-<h6 class="wp-block-heading has-text-color" style="color:#fffffd;font-size:1.13rem;font-style:normal;font-weight:600;line-height:1.3;text-transform:capitalize"><?php printf( esc_html__( '%s', 'variations' ), __( 'Our Work', 'variations' ) ); ?></h6>
-<!-- /wp:heading -->
-
-<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#fffffd"}}},"typography":{"fontSize":"1.13rem","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8","textDecoration":"none"},"spacing":{"margin":{"top":"1.56rem"}}}} -->
-<p class="has-link-color" style="margin-top:1.56rem;font-size:1.13rem;font-style:normal;font-weight:400;line-height:1.8;text-decoration:none"><a href="#"><?php printf( esc_html__( '%s', 'variations' ), __( 'About', 'variations' ) ); ?></a></p>
+<!-- wp:column {"verticalAlignment":"center","width":"","layout":{"type":"constrained"}} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"layout":{"type":"constrained","justifyContent":"center","contentSize":""}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontSize":"0.9rem","lineHeight":"1.2","fontStyle":"normal","fontWeight":"400"},"color":{"text":"#525060"},"elements":{"link":{"color":{"text":"#525060"}}}}} -->
+<p class="has-text-color has-link-color" style="color:#525060;font-size:0.9rem;font-style:normal;font-weight:400;line-height:1.2"><a href="#"><?php printf( esc_html__( '%s', 'variations' ), __( 'Store Policy', 'variations' ) ); ?></a></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#fffffd"}}},"typography":{"fontSize":"1.13rem","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"spacing":{"margin":{"top":"1.25rem"}}}} -->
-<p class="has-link-color" style="margin-top:1.25rem;font-size:1.13rem;font-style:normal;font-weight:400;line-height:1.8"><a href="#"><?php printf( esc_html__( '%s', 'variations' ), __( 'News', 'variations' ) ); ?></a></p>
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"0.9rem","lineHeight":"1.2","fontStyle":"normal","fontWeight":"400"},"color":{"text":"#525060"},"elements":{"link":{"color":{"text":"#525060"}}}}} -->
+<p class="has-text-color has-link-color" style="color:#525060;font-size:0.9rem;font-style:normal;font-weight:400;line-height:1.2"><a href="#"><?php printf( esc_html__( '%s', 'variations' ), __( 'Shipping &amp; Returns', 'variations' ) ); ?></a></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#fffffd"}}},"typography":{"fontSize":"1.13rem","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"spacing":{"margin":{"top":"1.25rem"}}}} -->
-<p class="has-link-color" style="margin-top:1.25rem;font-size:1.13rem;font-style:normal;font-weight:400;line-height:1.8"><a href="#"><?php printf( esc_html__( '%s', 'variations' ), __( 'Read Me', 'variations' ) ); ?></a></p>
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"0.9rem","lineHeight":"1.2","fontStyle":"normal","fontWeight":"400"},"color":{"text":"#525060"},"elements":{"link":{"color":{"text":"#525060"}}}}} -->
+<p class="has-text-color has-link-color" style="color:#525060;font-size:0.9rem;font-style:normal;font-weight:400;line-height:1.2"><a href="#"><?php printf( esc_html__( '%s', 'variations' ), __( 'FAQ', 'variations' ) ); ?></a></p>
 <!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"verticalAlignment":"top","width":"376px"} -->
-<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:376px"><!-- wp:heading {"level":6,"style":{"color":{"text":"#fffffd"},"typography":{"fontStyle":"normal","fontWeight":"600","fontSize":"1.13rem","lineHeight":"1.3","textTransform":"capitalize"}}} -->
-<h6 class="wp-block-heading has-text-color" style="color:#fffffd;font-size:1.13rem;font-style:normal;font-weight:600;line-height:1.3;text-transform:capitalize"><?php printf( esc_html__( '%s', 'variations' ), __( 'Travel', 'variations' ) ); ?></h6>
-<!-- /wp:heading -->
+<!-- wp:column {"verticalAlignment":"center","width":"","layout":{"type":"constrained"}} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"layout":{"type":"constrained","justifyContent":"center","contentSize":""}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontSize":"0.9rem","lineHeight":"1.2","fontStyle":"normal","fontWeight":"400"},"color":{"text":"#525060"},"elements":{"link":{"color":{"text":"#525060"}}}}} -->
+<p class="has-text-color has-link-color" style="color:#525060;font-size:0.9rem;font-style:normal;font-weight:400;line-height:1.2"><a href="#"><?php printf( esc_html__( '%s', 'variations' ), __( 'Store Policy', 'variations' ) ); ?></a></p>
+<!-- /wp:paragraph -->
 
-<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#fffffd"}}},"typography":{"fontSize":"1.13rem","fontStyle":"normal","fontWeight":"400","lineHeight":"1.5"},"spacing":{"margin":{"top":"1.56rem"}}}} -->
-<p class="has-link-color" style="margin-top:1.56rem;font-size:1.13rem;font-style:normal;font-weight:400;line-height:1.5"><?php printf( 
-    esc_html__( '%s Maria Therein Platz, NY %s %s', 'variations' ),
-    '<a href="mailto:' . __( 'mail@Servo.com', 'variations' ) . '">' . __( 'mail@Servo.com', 'variations' ) . '</a>',
-    '<a href="tel:' . __( '154-568-145', 'variations' ) . '">' . __( '154-568-145', 'variations' ) . '</a>',
-    '<a href="tel:' . __( '442-000', 'variations' ) . '">' . __( '442-000', 'variations' ) . '</a>',
-); ?></p>
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"0.9rem","lineHeight":"1.2","fontStyle":"normal","fontWeight":"400"},"color":{"text":"#525060"},"elements":{"link":{"color":{"text":"#525060"}}}}} -->
+<p class="has-text-color has-link-color" style="color:#525060;font-size:0.9rem;font-style:normal;font-weight:400;line-height:1.2"><a href="#"><?php printf( esc_html__( '%s', 'variations' ), __( 'Shipping &amp; Returns', 'variations' ) ); ?></a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"0.9rem","lineHeight":"1.2","fontStyle":"normal","fontWeight":"400"},"color":{"text":"#525060"},"elements":{"link":{"color":{"text":"#525060"}}}}} -->
+<p class="has-text-color has-link-color" style="color:#525060;font-size:0.9rem;font-style:normal;font-weight:400;line-height:1.2"><a href="#"><?php printf( esc_html__( '%s', 'variations' ), __( 'FAQ', 'variations' ) ); ?></a></p>
 <!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"","layout":{"type":"constrained"}} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"layout":{"type":"constrained","justifyContent":"center","contentSize":""}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontSize":"0.9rem","lineHeight":"1.2","fontStyle":"normal","fontWeight":"400"},"color":{"text":"#525060"},"elements":{"link":{"color":{"text":"#525060"}}}}} -->
+<p class="has-text-color has-link-color" style="color:#525060;font-size:0.9rem;font-style:normal;font-weight:400;line-height:1.2"><a href="#"><?php printf( esc_html__( '%s', 'variations' ), __( 'Facebook', 'variations' ) ); ?></a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"0.9rem","lineHeight":"1.2","fontStyle":"normal","fontWeight":"400"},"color":{"text":"#525060"},"elements":{"link":{"color":{"text":"#525060"}}}}} -->
+<p class="has-text-color has-link-color" style="color:#525060;font-size:0.9rem;font-style:normal;font-weight:400;line-height:1.2"><a href="#"><?php printf( esc_html__( '%s', 'variations' ), __( 'Instagram', 'variations' ) ); ?></a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"0.9rem","lineHeight":"1.2","fontStyle":"normal","fontWeight":"400"},"color":{"text":"#525060"},"elements":{"link":{"color":{"text":"#525060"}}}}} -->
+<p class="has-text-color has-link-color" style="color:#525060;font-size:0.9rem;font-style:normal;font-weight:400;line-height:1.2"><a href="#"><?php printf( esc_html__( '%s', 'variations' ), __( 'Pinterest', 'variations' ) ); ?></a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
 <!-- /wp:column --></div>
-<!-- /wp:columns --></div>
+<!-- /wp:columns -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"0.9rem","fontStyle":"normal","fontWeight":"400","lineHeight":"1.2"},"color":{"text":"#525060"}}} -->
+<p class="has-text-align-center has-text-color" style="color:#525060;font-size:0.9rem;font-style:normal;font-weight:400;line-height:1.2">© 2023 Antonino. All rights reserved.</p>
+<!-- /wp:paragraph --></div>
 <!-- /wp:group -->
--- patterns/Fashion-Hero.php
+++ patterns/Fashion-Hero.php
@@ -8,23 +8,23 @@
  */
 
 ?>
-<!-- wp:cover {"url":"http://variations.local/wp-content/uploads/2023/10/fashion-banner.jpg","id":573,"dimRatio":0,"overlayColor":"contrast","focalPoint":{"x":0.29,"y":0.17},"minHeightUnit":"vh","contentPosition":"center center","isDark":false,"tagName":"section","align":"full","layout":{"type":"constrained","contentSize":"px"}} -->
-<section class="wp-block-cover alignfull is-light"><span aria-hidden="true" class="wp-block-cover__background has-contrast-background-color has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-573" alt="" src="http://variations.local/wp-content/uploads/2023/10/fashion-banner.jpg" style="object-position:29% 17%" data-object-fit="cover" data-object-position="29% 17%"/><div class="wp-block-cover__inner-container"><!-- wp:spacer {"height":"200px"} -->
+<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/fashion/fashion-banner.jpg","id":573,"dimRatio":0,"overlayColor":"contrast","focalPoint":{"x":0.94,"y":0},"minHeightUnit":"vh","contentPosition":"center center","isDark":false,"tagName":"section","align":"full","layout":{"type":"constrained","contentSize":"px"}} -->
+<section class="wp-block-cover alignfull is-light"><span aria-hidden="true" class="wp-block-cover__background has-contrast-background-color has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-573" alt="" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/fashion/fashion-banner.jpg" style="object-position:94% 0%" data-object-fit="cover" data-object-position="94% 0%"/><div class="wp-block-cover__inner-container"><!-- wp:spacer {"height":"200px"} -->
 <div style="height:200px" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:group {"layout":{"type":"constrained","contentSize":"1200px"}} -->
-<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":6,"style":{"typography":{"fontSize":"3rem","fontStyle":"normal","fontWeight":"400"},"color":{"text":"#525060"}}} -->
-<h6 class="wp-block-heading has-text-align-center has-text-color" style="color:#525060;font-size:3rem;font-style:normal;font-weight:400">We are</h6>
+<!-- wp:group {"style":{"typography":{"fontSize":"1.1rem","fontStyle":"normal","fontWeight":"400","lineHeight":"1.5"}},"layout":{"type":"constrained","contentSize":"1140px"}} -->
+<div class="wp-block-group" style="font-size:1.1rem;font-style:normal;font-weight:400;line-height:1.5"><!-- wp:heading {"textAlign":"center","level":6,"style":{"typography":{"fontSize":"3rem","fontStyle":"normal","fontWeight":"400"},"color":{"text":"#525060"}}} -->
+<h6 class="wp-block-heading has-text-align-center has-text-color" style="color:#525060;font-size:3rem;font-style:normal;font-weight:400"><?php printf( esc_html__( '%s', 'variations' ), __( 'We are', 'variations' ) ); ?></h6>
 <!-- /wp:heading -->
 
 <!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"4.7rem","fontStyle":"normal","fontWeight":"400","textTransform":"uppercase"},"color":{"text":"#525060"},"spacing":{"margin":{"top":"0px","bottom":"0px"}}}} -->
-<h2 class="wp-block-heading has-text-align-center has-text-color" style="color:#525060;margin-top:0px;margin-bottom:0px;font-size:4.7rem;font-style:normal;font-weight:400;text-transform:uppercase">Create Style</h2>
+<h2 class="wp-block-heading has-text-align-center has-text-color" style="color:#525060;margin-top:0px;margin-bottom:0px;font-size:4.7rem;font-style:normal;font-weight:400;text-transform:uppercase"><?php printf( esc_html__( '%s', 'variations' ), __( 'Create Style', 'variations' ) ); ?></h2>
 <!-- /wp:heading -->
 
-<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"margin":{"top":"3rem","bottom":"3rem"}},"typography":{"fontSize":"1.1rem"}}} -->
-<div class="wp-block-buttons has-custom-font-size" style="margin-top:3rem;margin-bottom:3rem;font-size:1.1rem"><!-- wp:button {"textColor":"base","style":{"spacing":{"padding":{"left":"3.5rem","right":"3.5rem","top":"0.7rem","bottom":"0.7rem"}},"color":{"background":"#696778"},"typography":{"fontSize":"1.1rem","lineHeight":"1.5"},"border":{"radius":"0px"}},"className":"is-style-fill"} -->
-<div class="wp-block-button has-custom-font-size is-style-fill" style="font-size:1.1rem;line-height:1.5"><a class="wp-block-button__link has-base-color has-text-color has-background wp-element-button" style="border-radius:0px;background-color:#696778;padding-top:0.7rem;padding-right:3.5rem;padding-bottom:0.7rem;padding-left:3.5rem">SHOP NOW</a></div>
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"margin":{"top":"3rem","bottom":"3rem"}},"typography":{"fontSize":"1.1rem","lineHeight":"1.5"}}} -->
+<div class="wp-block-buttons has-custom-font-size" style="margin-top:3rem;margin-bottom:3rem;font-size:1.1rem;line-height:1.5"><!-- wp:button {"style":{"spacing":{"padding":{"left":"3.5rem","right":"3.5rem","top":"0.9rem","bottom":"0.9rem"}},"color":{"background":"#696778","text":"#fffffd"},"typography":{"fontSize":"1.1rem","lineHeight":"1.1","fontStyle":"normal","fontWeight":"400"},"border":{"radius":"0px"}},"className":"is-style-fill"} -->
+<div class="wp-block-button has-custom-font-size is-style-fill" style="font-size:1.1rem;font-style:normal;font-weight:400;line-height:1.1"><a class="wp-block-button__link has-text-color has-background wp-element-button" style="border-radius:0px;color:#fffffd;background-color:#696778;padding-top:0.9rem;padding-right:3.5rem;padding-bottom:0.9rem;padding-left:3.5rem"><?php printf( esc_html__( '%s', 'variations' ), __( 'SHOP NOW', 'variations' ) ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group -->
