5430fa435f7e0654249914023f138bc433faf778
Olena Iaroshynska <olenaiaroshynska@gmail.com>
2023-10-20 19:30:21 +0300

    Added header and 3 sections Consalting Green template.

---
+++ patterns/Consulting-Green-2-Columns.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Title: Consulting Green 2 Columns
+ * Slug: variations/consulting-green-2-columns
+ * Description: Add a Consulting Green 2 Columns section
+ * Categories: text, column
+ * Keywords: columns, text
+ */
+
+?>
+<!-- wp:group {"tagName":"section","align":"full","style":{"spacing":{"padding":{"top":"5.01rem","bottom":"5.5rem","left":"1rem","right":"1rem"},"margin":{"top":"0px"}},"color":{"text":"#3e3e3e"},"typography":{"fontSize":"1.13rem","fontStyle":"normal","fontWeight":"400","lineHeight":"1.4"}},"layout":{"type":"constrained","contentSize":"1140px"}} -->
+<section class="wp-block-group alignfull has-text-color" style="color:#3e3e3e;margin-top:0px;padding-top:5.01rem;padding-right:1rem;padding-bottom:5.5rem;padding-left:1rem;font-size:1.13rem;font-style:normal;font-weight:400;line-height:1.4"><!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"2rem","left":"2.5rem"},"margin":{"bottom":"0px"}}}} -->
+<div class="wp-block-columns" style="margin-bottom:0px"><!-- wp:column {"width":"45%","style":{"spacing":{"padding":{"right":"8%","top":"2%"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-column" style="padding-top:2%;padding-right:8%;flex-basis:45%"><!-- wp:heading {"textAlign":"left","level":5,"style":{"color":{"text":"#002d19"},"typography":{"fontSize":"1.01rem","fontStyle":"normal","fontWeight":"500","lineHeight":"1.4","textTransform":"uppercase"},"spacing":{"margin":{"bottom":"0rem"}}}} -->
+<h5 class="wp-block-heading has-text-align-left has-text-color" style="color:#002d19;margin-bottom:0rem;font-size:1.01rem;font-style:normal;font-weight:500;line-height:1.4;text-transform:uppercase">About us</h5>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"left","style":{"color":{"text":"#002d19"},"typography":{"fontSize":"1.9rem","fontStyle":"normal","fontWeight":"600","lineHeight":"1.3","letterSpacing":"0px"},"spacing":{"margin":{"bottom":"1.01rem","top":"0.5rem"}}}} -->
+<h2 class="wp-block-heading has-text-align-left has-text-color" style="color:#002d19;margin-top:0.5rem;margin-bottom:1.01rem;font-size:1.9rem;font-style:normal;font-weight:600;letter-spacing:0px;line-height:1.3">We have different experts for different financial services duis up tincidunt est aenean molestie purus odio.</h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"layout":{"type":"constrained","contentSize":"518px","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"left","style":{"typography":{"fontSize":"0.9rem","fontStyle":"normal","fontWeight":"400","lineHeight":"1.6"},"spacing":{"margin":{"top":"0px","bottom":"1.77rem"}},"color":{"text":"#3d3d3d"}}} -->
+<p class="has-text-align-left has-text-color" style="color:#3d3d3d;margin-top:0px;margin-bottom:1.77rem;font-size:0.9rem;font-style:normal;font-weight:400;line-height:1.6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi iaculis finibus auctor. Phasellus accumsan luctus ligula, eu rutrum eros rutrum ut. Vivamus quis semper lacus. Maecenas et iaculis nisl. Etiam mollis dapibus leo, non malesuada nisi molestie ut. </p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"55%","layout":{"type":"default"}} -->
+<div class="wp-block-column" style="flex-basis:55%"><!-- wp:image {"align":"center","id":882,"width":"580px","sizeSlug":"full","linkDestination":"custom"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://variations.local/wp-content/uploads/2023/10/consulting-green-about.jpg" alt="" class="wp-image-882" style="width:580px"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></section>
+<!-- /wp:group -->
