8c50c6b2c732f8584e29788b3b28088e5d62ad29
Maksym Marko <markomaksym@gmail.com>
2023-10-20 18:43:07 +0300

    Mind template started.

---
+++ patterns/Mind-3-columns-text.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * Title: Mind 3 Columns with text
+ * Slug: variations/mind-3-columns-text
+ * Description: Add a Mind 3 Columns with text section
+ * Categories: column
+ * Keywords: 3 columns
+ */
+
+?>
+<!-- wp:group {"tagName":"section","align":"full","style":{"spacing":{"padding":{"top":"1rem","bottom":"7rem","left":"1rem","right":"1rem"},"margin":{"top":"0px"}},"color":{"text":"#3e3e3e"},"typography":{"fontSize":"1.13rem","fontStyle":"normal","fontWeight":"400","lineHeight":"1.4"},"border":{"width":"0px","style":"none"}},"layout":{"type":"constrained","contentSize":"1200px"}} -->
+<section class="wp-block-group alignfull has-text-color" style="border-style:none;border-width:0px;color:#3e3e3e;margin-top:0px;padding-top:1rem;padding-right:1rem;padding-bottom:7rem;padding-left:1rem;font-size:1.13rem;font-style:normal;font-weight:400;line-height:1.4"><!-- wp:group {"style":{"spacing":{"padding":{"bottom":"2rem"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-bottom:2rem"><!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"2.4rem"}}} -->
+<h2 class="wp-block-heading has-text-align-center" style="font-size:2.4rem">How can I help you?</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"padding":{"bottom":"1rem"}}}} -->
+<div class="wp-block-column" style="padding-bottom:1rem"><!-- wp:columns {"isStackedOnMobile":false,"style":{"spacing":{"padding":{"right":"0","left":"0"},"blockGap":{"top":"0","left":"1rem"}}}} -->
+<div class="wp-block-columns is-not-stacked-on-mobile" style="padding-right:0;padding-left:0"><!-- wp:column {"width":"80px"} -->
+<div class="wp-block-column" style="flex-basis:80px"><!-- wp:image {"id":667,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/mind/icon1.png" alt="" class="wp-image-667"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"1rem"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"1.7rem"}}} -->
+<h3 class="wp-block-heading" style="font-size:1.7rem">Love issues</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"1.13rem"}}} -->
+<p style="font-size:1.13rem">Lorem ipsum dolor sit amet, conse ctetur adipiscing elit. Aenean vitae ex vitae risus varius malesuada. Nullam congue egestas</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#e27013"}}},"typography":{"fontSize":"1.07rem"}}} -->
+<p class="has-link-color" style="font-size:1.07rem"><a href="#">READ MORE</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"bottom":"1rem"}}}} -->
+<div class="wp-block-column" style="padding-bottom:1rem"><!-- wp:columns {"isStackedOnMobile":false,"style":{"spacing":{"padding":{"right":"0","left":"0"},"blockGap":{"top":"0","left":"1rem"}}}} -->
+<div class="wp-block-columns is-not-stacked-on-mobile" style="padding-right:0;padding-left:0"><!-- wp:column {"width":"80px"} -->
+<div class="wp-block-column" style="flex-basis:80px"><!-- wp:image {"id":680,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/mind/icon2.png" alt="" class="wp-image-680"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"1rem"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"1.7rem"}}} -->
+<h3 class="wp-block-heading" style="font-size:1.7rem">Health Goals</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"1.13rem"}}} -->
+<p style="font-size:1.13rem">Lorem ipsum dolor sit amet, conse ctetur adipiscing elit. Aenean vitae ex vitae risus varius malesuada. Nullam congue egestas</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#e27013"}}},"typography":{"fontSize":"1.07rem"}}} -->
+<p class="has-link-color" style="font-size:1.07rem"><a href="#">READ MORE</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"bottom":"1rem"}}}} -->
+<div class="wp-block-column" style="padding-bottom:1rem"><!-- wp:columns {"isStackedOnMobile":false,"style":{"spacing":{"padding":{"right":"0","left":"0"},"blockGap":{"top":"0","left":"1rem"}}}} -->
+<div class="wp-block-columns is-not-stacked-on-mobile" style="padding-right:0;padding-left:0"><!-- wp:column {"width":"80px"} -->
+<div class="wp-block-column" style="flex-basis:80px"><!-- wp:image {"id":681,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/mind/icon3.png" alt="" class="wp-image-681"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"1rem"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"1.7rem"}}} -->
+<h3 class="wp-block-heading" style="font-size:1.7rem">Stress</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"1.13rem"}}} -->
+<p style="font-size:1.13rem">Lorem ipsum dolor sit amet, conse ctetur adipiscing elit. Aenean vitae ex vitae risus varius malesuada. Nullam congue egestas</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#e27013"}}},"typography":{"fontSize":"1.07rem"}}} -->
+<p class="has-link-color" style="font-size:1.07rem"><a href="#">READ MORE</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"style":{"spacing":{"margin":{"top":"4rem","bottom":"0"}}}} -->
+<div class="wp-block-columns" style="margin-top:4rem;margin-bottom:0"><!-- wp:column {"style":{"spacing":{"padding":{"bottom":"1rem"}}}} -->
+<div class="wp-block-column" style="padding-bottom:1rem"><!-- wp:columns {"isStackedOnMobile":false,"style":{"spacing":{"padding":{"right":"0","left":"0"},"blockGap":{"top":"0","left":"1rem"}}}} -->
+<div class="wp-block-columns is-not-stacked-on-mobile" style="padding-right:0;padding-left:0"><!-- wp:column {"width":"80px"} -->
+<div class="wp-block-column" style="flex-basis:80px"><!-- wp:image {"id":685,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/mind/icon4.png" alt="" class="wp-image-685"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"1rem"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"1.7rem"}}} -->
+<h3 class="wp-block-heading" style="font-size:1.7rem">Communication</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"1.13rem"}}} -->
+<p style="font-size:1.13rem">Lorem ipsum dolor sit amet, conse ctetur adipiscing elit. Aenean vitae ex vitae risus varius malesuada. Nullam congue egestas</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#e27013"}}},"typography":{"fontSize":"1.07rem"}}} -->
+<p class="has-link-color" style="font-size:1.07rem"><a href="#">READ MORE</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"bottom":"1rem"}}}} -->
+<div class="wp-block-column" style="padding-bottom:1rem"><!-- wp:columns {"isStackedOnMobile":false,"style":{"spacing":{"padding":{"right":"0","left":"0"},"blockGap":{"top":"0","left":"1rem"}}}} -->
+<div class="wp-block-columns is-not-stacked-on-mobile" style="padding-right:0;padding-left:0"><!-- wp:column {"width":"80px"} -->
+<div class="wp-block-column" style="flex-basis:80px"><!-- wp:image {"id":686,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/mind/icon5.png" alt="" class="wp-image-686"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"1rem"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"1.7rem"}}} -->
+<h3 class="wp-block-heading" style="font-size:1.7rem">Creativity</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"1.13rem"}}} -->
+<p style="font-size:1.13rem">Lorem ipsum dolor sit amet, conse ctetur adipiscing elit. Aenean vitae ex vitae risus varius malesuada. Nullam congue egestas</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#e27013"}}},"typography":{"fontSize":"1.07rem"}}} -->
+<p class="has-link-color" style="font-size:1.07rem"><a href="#">READ MORE</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"bottom":"1rem"}}}} -->
+<div class="wp-block-column" style="padding-bottom:1rem"><!-- wp:columns {"isStackedOnMobile":false,"style":{"spacing":{"padding":{"right":"0","left":"0"},"blockGap":{"top":"0","left":"1rem"}}}} -->
+<div class="wp-block-columns is-not-stacked-on-mobile" style="padding-right:0;padding-left:0"><!-- wp:column {"width":"80px"} -->
+<div class="wp-block-column" style="flex-basis:80px"><!-- wp:image {"id":687,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/mind/icon6.png" alt="" class="wp-image-687"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"1rem"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"1.7rem"}}} -->
+<h3 class="wp-block-heading" style="font-size:1.7rem">Patterns</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"1.13rem"}}} -->
+<p style="font-size:1.13rem">Lorem ipsum dolor sit amet, conse ctetur adipiscing elit. Aenean vitae ex vitae risus varius malesuada. Nullam congue egestas</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#e27013"}}},"typography":{"fontSize":"1.07rem"}}} -->
+<p class="has-link-color" style="font-size:1.07rem"><a href="#">READ MORE</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></section>
+<!-- /wp:group -->
