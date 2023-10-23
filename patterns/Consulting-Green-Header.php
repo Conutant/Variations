66aa2fd0890cbe98555a342e11e38f617b835ada
Olena Iaroshynska <olenaiaroshynska@gmail.com>
2023-10-23 19:08:59 +0300

    Consulting Green Header conflict fixes.

--- patterns/Consulting-Green-Header.php
+++ patterns/Consulting-Green-Header.php
@@ -9,7 +9,6 @@
  */
 
 ?>
-
 <!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"},"spacing":{"padding":{"left":"1rem","right":"1rem","top":"2rem","bottom":"0rem"},"margin":{"top":"0","bottom":"0"}},"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"1.5"},"dimensions":{"minHeight":"0px"},"color":{"text":"#fffffd"}},"layout":{"type":"constrained","justifyContent":"center","contentSize":"1200px"}} -->
 <div class="wp-block-group has-text-color" style="color:#fffffd;min-height:0px;margin-top:0;margin-bottom:0;padding-top:2rem;padding-right:1rem;padding-bottom:0rem;padding-left:1rem;font-style:normal;font-weight:400;line-height:1.5"><!-- wp:columns {"style":{"spacing":{"padding":{"top":"0rem","bottom":"1.4rem"},"blockGap":{"top":"2rem","left":"0rem"}},"border":{"top":{"width":"0px","style":"none"},"right":{"width":"0px","style":"none"},"bottom":{"color":"#ffffff40","width":"1px"},"left":{"width":"0px","style":"none"}}}} -->
 <div class="wp-block-columns" style="border-top-style:none;border-top-width:0px;border-right-style:none;border-right-width:0px;border-bottom-color:#ffffff40;border-bottom-width:1px;border-left-style:none;border-left-width:0px;padding-top:0rem;padding-bottom:1.4rem"><!-- wp:column {"verticalAlignment":"center","width":"33%","style":{"spacing":{"padding":{"top":"0px","bottom":"0px","left":"0px","right":"0px"}}},"layout":{"type":"constrained","justifyContent":"center","contentSize":""}} -->
