c3e4eedb265c103c32b20c48b5bcb6232bd17807
Olena Iaroshynska <olenaiaroshynska@gmail.com>
2023-10-23 19:40:20 +0300

    Consuling Green Theme changes.

--- patterns/Consulting-Green-Footer.php
+++ patterns/Consulting-Green-Footer.php
@@ -9,8 +9,8 @@
  */
 
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|20","right":"var:preset|spacing|20"}},"color":{"background":"#00341e","text":"#fffffd"},"typography":{"fontStyle":"normal","fontWeight":"400"}},"layout":{"type":"constrained","contentSize":"890px"}} -->
-<div class="wp-block-group has-text-color has-background" style="color:#fffffd;background-color:#00341e;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--20);font-style:normal;font-weight:400"><!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":{"top":"2.01rem","left":"5.01rem"}}}} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"1.51rem","bottom":"1.51rem","left":"1rem","right":"1rem"}},"color":{"background":"#00341e","text":"#fffffd"},"typography":{"fontStyle":"normal","fontWeight":"400"}},"layout":{"type":"constrained","contentSize":"1000px"}} -->
+<div class="wp-block-group has-text-color has-background" style="color:#fffffd;background-color:#00341e;padding-top:1.51rem;padding-right:1rem;padding-bottom:1.51rem;padding-left:1rem;font-style:normal;font-weight:400"><!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":{"top":"2.01rem","left":"5.01rem"}}}} -->
 <div class="wp-block-columns alignwide" style="margin-top:0;margin-bottom:0"><!-- wp:column {"verticalAlignment":"center","width":"40%","layout":{"type":"constrained"}} -->
 <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:40%"><!-- wp:navigation {"ref":820,"customTextColor":"#fffffd","overlayMenu":"never","layout":{"type":"flex","justifyContent":"center","flexWrap":"nowrap"},"style":{"typography":{"fontSize":"0.9rem","lineHeight":"1.5","fontStyle":"normal","fontWeight":"400"}}} /--></div>
 <!-- /wp:column -->
@@ -19,8 +19,8 @@
 <div class="wp-block-column is-vertically-aligned-center" style="padding-right:0;padding-left:0;flex-basis:20%"><!-- wp:site-title {"textAlign":"center","style":{"color":{"text":"#fffffd"},"typography":{"fontSize":"1.81rem","lineHeight":"1.5","fontStyle":"normal","fontWeight":"400","textTransform":"capitalize"}}} /--></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"verticalAlignment":"center","width":"40%","layout":{"type":"constrained"}} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:40%"><!-- wp:paragraph {"align":"left","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"},":hover":{"color":{"text":"var:preset|color|base"}}}},"typography":{"fontSize":"0.9rem","lineHeight":"1.5","fontStyle":"normal","fontWeight":"400"},"color":{"text":"#fffffd"}}} -->
+<!-- wp:column {"verticalAlignment":"center","width":"40%","style":{"color":{"text":"#fffffd"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-column is-vertically-aligned-center has-text-color" style="color:#fffffd;flex-basis:40%"><!-- wp:paragraph {"align":"left","style":{"elements":{"link":{"color":{"text":"#fffffd"},":hover":{"color":{"text":"#fffffd"}}}},"typography":{"fontSize":"0.9rem","lineHeight":"1.5","fontStyle":"normal","fontWeight":"400"},"color":{"text":"#fffffd"}}} -->
 <p class="has-text-align-left has-text-color has-link-color" style="color:#fffffd;font-size:0.9rem;font-style:normal;font-weight:400;line-height:1.5">© 2023 Fnance. All rights reserved. copy</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column --></div>
