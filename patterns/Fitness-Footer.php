9bcc758a2bde3a15e733db0d4f76da15f62c08e3
Maksym Marko <markomaksym@gmail.com>
2023-10-24 14:54:04 +0300

    Fitness template started.

---
+++ patterns/Fitness-Footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Title: Fitness Footer
+ * Slug: variations/fitness-footer
+ * Description: Add a Footer With Site title, Columns and Copyright
+ * Categories: footer, column
+ * Keywords: footer
+ * Block Types: core/template-part/footer
+ */
+
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"52px","bottom":"52px"}},"elements":{"link":{"color":{"text":"var:preset|color|custom-color-3"}}}},"backgroundColor":"contrast","textColor":"custom-color-3","layout":{"type":"constrained","contentSize":""}} -->
+<div class="wp-block-group has-custom-color-3-color has-contrast-background-color has-text-color has-background has-link-color" style="padding-top:52px;padding-bottom:52px"><!-- wp:social-links {"customIconColor":"#f8f6f1","iconColorValue":"#f8f6f1","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center"}} -->
+<ul class="wp-block-social-links has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"#","service":"tiktok"} /--></ul>
+<!-- /wp:social-links -->
+
+<!-- wp:paragraph {"align":"center","style":{"color":{"text":"#f8f6f1"}}} -->
+<p class="has-text-align-center has-text-color" id="rfFT0L7YjgHXCIMo" style="color:#f8f6f1">Noah Schumacher 123 Anywhere St. Any City, ST 12345</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"12px"}},"color":{"text":"#f8f6f1"}},"fontSize":"xxx-small"} -->
+<p class="has-text-align-center has-text-color has-xxx-small-font-size" style="color:#f8f6f1;margin-top:12px">© Copyright 2023 All Right Reserved.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
