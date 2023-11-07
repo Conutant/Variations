5e6682064d95ea35d877dab8c4c63cabb338f38e
Maksym Marko <markomaksym@gmail.com>
2023-11-07 15:49:35 +0200

    Sky template created.

---
+++ patterns/Sky-Footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Title: Sky Footer
+ * Slug: variations/sky-footer
+ * Description: Add a Footer With Site title, Columns and Copyright
+ * Categories: footer, column
+ * Keywords: footer
+ * Block Types: core/template-part/footer
+ */
+
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"52px","bottom":"52px","left":"30px","right":"30px"},"margin":{"top":"0px","bottom":"0px"}},"elements":{"link":{"color":{"text":"#ffffff"}}},"color":{"background":"#000000","text":"#ffffff"}},"layout":{"type":"constrained","contentSize":""}} -->
+<div class="wp-block-group has-text-color has-background has-link-color" style="color:#ffffff;background-color:#000000;margin-top:0px;margin-bottom:0px;padding-top:52px;padding-right:30px;padding-bottom:52px;padding-left:30px"><!-- wp:social-links {"customIconColor":"#f8f6f1","iconColorValue":"#f8f6f1","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center"}} -->
+<ul class="wp-block-social-links has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"#","service":"tiktok"} /--></ul>
+<!-- /wp:social-links -->
+
+<!-- wp:paragraph {"align":"center","style":{"color":{"text":"#f8f6f1"}}} -->
+<p class="has-text-align-center has-text-color" id="rfFT0L7YjgHXCIMo" style="color:#f8f6f1">Liam Benjamin 123 Anywhere St. Any City, ST 12345</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"12px"}},"color":{"text":"#f8f6f1"}},"fontSize":"xxx-small"} -->
+<p class="has-text-align-center has-text-color has-xxx-small-font-size" style="color:#f8f6f1;margin-top:12px">© Copyright 2024 All Right Reserved.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
