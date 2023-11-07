c360120a189b3c515cc3528ec9afa3a1cae081cc
Olena Iaroshynska <olenaiaroshynska@gmail.com>
2023-11-07 09:26:55 +0200

    Created Stone Template.

---
+++ patterns/Stone-Footer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: Stone Footer
+ * Slug: variations/stone-footer
+ * Description: Add a Footer With Site Title, Columns and Copyright
+ * Categories: footer, column
+ * Keywords: footer
+ * Block Types: core/template-part/footer
+ */
+
+?>
---
+++ patterns/Stone-Header.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: Stone Header
+ * Slug: variations/stone-header
+ * Description: Header with site title and navigation
+ * Categories: header
+ * Keywords: header, nav, site logo
+ * Block Types: core/template-part/header
+ */
+
+?>
