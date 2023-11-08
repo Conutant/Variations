c5265af4f05c636757dbc6f179268587905d5279
Olena Iaroshynska <olenaiaroshynska@gmail.com>
2023-11-08 16:56:27 +0200

    Pebble header and footer.

---
+++ patterns/Pebble-Header.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Pebble Header
+ * Slug: variations/pebble-header
+ * Description: Header with site title and navigation
+ * Categories: header
+ * Keywords: header, nav, site title
+ * Block Types: core/template-part/header
+ */
+
+?>
+<!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"},"spacing":{"padding":{"top":"30px","bottom":"30px","left":"30px","right":"30px"},"margin":{"top":"0","bottom":"0"}},"color":{"text":"#422f01"},"elements":{"link":{"color":{"text":"#422f01"}}}},"layout":{"type":"constrained","contentSize":"1200px"}} -->
+<div class="wp-block-group has-text-color has-link-color" style="color:#422f01;margin-top:0;margin-bottom:0;padding-top:30px;padding-right:30px;padding-bottom:30px;padding-left:30px"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:site-title {"style":{"color":{"text":"#422f01"},"elements":{"link":{"color":{"text":"#422f01"}}},"typography":{"fontSize":"28px","fontStyle":"normal","fontWeight":"700"}}} /-->
+
+<!-- wp:navigation {"customTextColor":"#422f01","icon":"menu","layout":{"type":"flex","justifyContent":"center"},"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"500"}},"fontFamily":"system-font"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
