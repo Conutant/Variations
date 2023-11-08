255d054a6a24460a5068a33e5b88d0daf679e2bc
Maksym Marko <markomaksym@gmail.com>
2023-11-08 14:29:31 +0200

    Peak template started.

---
+++ patterns/Peak-Footer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Title: Peak Footer
+ * Slug: variations/peak-footer
+ * Description: Add a Footer With Menu, Site title and Copyright
+ * Categories: footer, column
+ * Keywords: footer
+ * Block Types: core/template-part/footer
+ */
+
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"52px","bottom":"52px"}},"elements":{"link":{"color":{"text":"#000000"}}},"color":{"background":"#fd9f5b","text":"#000000"}},"layout":{"type":"constrained","contentSize":"1200px"}} -->
+<div class="wp-block-group has-text-color has-background has-link-color" style="color:#000000;background-color:#fd9f5b;padding-top:52px;padding-bottom:52px"><!-- wp:columns {"verticalAlignment":"center"} -->
+<div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"blockGap":"4px"}}} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:paragraph {"align":"left","textColor":"custom-color-1"} -->
+<p class="has-text-align-left has-custom-color-1-color has-text-color" id="rfFT0L7YjgHXCIMo">Peak 123 Anywhere St. Any City, ST 12345</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:social-links {"iconColor":"custom-color-1","iconColorValue":"#212529","size":"has-small-icon-size","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"right"}} -->
+<ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"#","service":"tiktok"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
---
+++ patterns/Peak-Header.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Title: Green Header
+ * Slug: variations/peak-header
+ * Description: Header with site title, navigation and button
+ * Categories: header
+ * Keywords: header, nav, site title
+ * Block Types: core/template-part/header
+ */
+
+?>
+<!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"},"spacing":{"padding":{"top":"30px","bottom":"30px","left":"30px","right":"30px"},"margin":{"top":"0","bottom":"0"}},"color":{"text":"#212529"},"elements":{"link":{"color":{"text":"#212529"}}}},"layout":{"type":"constrained","contentSize":"1200px"}} -->
+<div class="wp-block-group has-text-color has-link-color" style="color:#212529;margin-top:0;margin-bottom:0;padding-top:30px;padding-right:30px;padding-bottom:30px;padding-left:30px"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0"},"layout":{"selfStretch":"fill","flexSize":null}},"textColor":"custom-color-1","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group has-custom-color-1-color has-text-color"><!-- wp:site-title {"level":2,"style":{"typography":{"fontSize":"29px","fontStyle":"normal","fontWeight":"700"}}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:navigation {"ref":286,"customTextColor":"#212529","layout":{"type":"flex","justifyContent":"center"},"style":{"typography":{"fontSize":"16px","textTransform":"uppercase","fontStyle":"normal","fontWeight":"500"}}} /-->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"600","textTransform":"uppercase"},"border":{"radius":"16px","width":"2px","style":"solid","color":"#212529"},"spacing":{"padding":{"left":"48px","right":"48px","top":"12px","bottom":"12px"}},"color":{"text":"#212529","background":"#adb0ff"}}} -->
+<div class="wp-block-button has-custom-font-size" style="font-size:16px;font-style:normal;font-weight:600;text-transform:uppercase"><a class="wp-block-button__link has-text-color has-background has-border-color wp-element-button" style="border-color:#212529;border-style:solid;border-width:2px;border-radius:16px;color:#212529;background-color:#adb0ff;padding-top:12px;padding-right:48px;padding-bottom:12px;padding-left:48px"><strong><strong>all sessions</strong></strong></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
