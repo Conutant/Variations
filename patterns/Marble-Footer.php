63e1949892a5c355b894c7d3141096e169e166e7
Olena Iaroshynska <olenaiaroshynska@gmail.com>
2023-11-09 09:42:21 +0200

    Marble  header and footer.

---
+++ patterns/Marble-Footer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: Marble Footer
+ * Slug: variations/marble-footer
+ * Description: Add a Marble Footer
+ * Categories: footer
+ * Keywords: footer
+ * Block Types: core/template-part/footer
+ */
+
+?>
---
+++ patterns/Marble-Header.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: Marble Header
+ * Slug: variations/marble-header
+ * Description: Header with site title and navigation
+ * Categories: header
+ * Keywords: header, nav, site title
+ * Block Types: core/template-part/header
+ */
+
+?>
