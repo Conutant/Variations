a620869e040ae4c21484e7afde42586f59e73508
Olena Iaroshynska <olenaiaroshynska@gmail.com>
2023-11-13 13:04:58 +0200

    Theme json changes (Cube Template added).

---
+++ patterns/Cube-Footer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: Cube Footer
+ * Slug: variations/cube-footer
+ * Description: Add a footer with Copyright
+ * Categories: footer, column
+ * Keywords: footer
+ * Block Types: core/template-part/footer
+ */
+
+?>
--- patterns/Tasty-Header.php
+++ patterns/Tasty-Header.php
@@ -13,7 +13,7 @@
 <div class="wp-block-group" style="border-bottom-color:#000000;border-bottom-width:1px;margin-top:0;margin-bottom:0;padding-top:30px;padding-right:1rem;padding-bottom:30px;padding-left:1rem"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group"><!-- wp:site-title {"style":{"color":{"text":"#ffffff"},"elements":{"link":{"color":{"text":"#ffffff"},":hover":{"color":{"text":"#ffffff"}}}},"typography":{"fontSize":"30px","fontStyle":"normal","fontWeight":"500","textTransform":"uppercase","letterSpacing":"2.5px"}}} /-->
 
-<!-- wp:navigation {"ref":820,"textColor":"white","icon":"menu","customOverlayBackgroundColor":"#096969","overlayTextColor":"white","layout":{"type":"flex","justifyContent":"center"},"style":{"typography":{"fontSize":"16px","letterSpacing":"1px","textTransform":"uppercase","fontStyle":"normal","fontWeight":"700"}},"fontFamily":"bayon"} /-->
+<!-- wp:navigation {"textColor":"white","icon":"menu","customOverlayBackgroundColor":"#096969","overlayTextColor":"white","layout":{"type":"flex","justifyContent":"center"},"style":{"typography":{"fontSize":"16px","letterSpacing":"1px","textTransform":"uppercase","fontStyle":"normal","fontWeight":"700"}},"fontFamily":"bayon"} /-->
 
 <!-- wp:buttons -->
 <div class="wp-block-buttons"><!-- wp:button {"style":{"color":{"background":"#fce347","text":"#000000"},"typography":{"fontSize":"18px","textTransform":"uppercase"},"border":{"radius":"50px"},"spacing":{"padding":{"left":"30px","right":"30px","top":"10px","bottom":"10px"}}},"fontFamily":"bayon"} -->
