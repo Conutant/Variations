e47b7bc0807c5ac7ec78451f5cfef487130fc6c9
Maksym Marko <markomaksym@gmail.com>
2024-02-06 12:22:22 +0200

    Header Sticky manager.

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -4,25 +4,102 @@
         /**
          * Toggle Sub Menu
          */
-        $('.wp-block-navigation-submenu__toggle, .wp-block-navigation-item.has-child > a').on('click', function (e) {
+        window.variationsMenuManager = window.variationsMenuManager || {
 
-            if ($(window).width() <= 767) {
+            subMenuElements: '.wp-block-navigation-submenu__toggle, .wp-block-navigation-item.has-child > a',
+            menuLinks: '.wp-block-navigation a',
+            closeButton: '.wp-block-navigation__responsive-container-close',
 
-                e.preventDefault();
-                $(this).parent().children('ul').slideToggle('fast');
+            checkMenuLinks: function () {
+
+                const _this = this;
+
+                $(this.menuLinks).on('click', function () {
+
+                    const href = $(this).attr('href');
+                    const regex = new RegExp('#');
+
+                    if (regex.test(href)) {
+
+                        $(_this.closeButton).trigger('click');
+                    }
+                });
+            },
+
+            bindToggleSubMenu: function () {
+
+                $(this.subMenuElements).on('click', function (e) {
+
+                    if ($(window).width() <= 767) {
+
+                        e.preventDefault();
+                        $(this).parent().children('ul').slideToggle('fast');
+                    }
+                });
+            },
+
+            init: function () {
+
+                // Toggle SubMenu
+                this.bindToggleSubMenu();
+
+                // Close Menu on mobile if there is a #
+                this.checkMenuLinks();
             }
-        });
+        }
 
-        $('.wp-block-navigation a').on('click', function () {
+        variationsMenuManager.init();
 
-            const href = $(this).attr('href');
-            const regex = new RegExp('#');
+        /**
+         * Sticky Header
+         */
+        window.variationsManageStickyHeader = window.variationsManageStickyHeader || {
 
-            if (regex.test(href)) {
+            header: '.is-position-sticky',
+            cleanClass: 'mx_clean_header_background',
 
-                $('.wp-block-navigation__responsive-container-close').trigger('click');
+            manageHeader: function () {
+
+                if ($(this.header).length === 0) return;
+
+                const window_width = $(window).width();
+
+                if (window_width < 768) {
+
+                    $(this.header).removeClass(this.cleanClass);
+                    return;
+                }
+
+                const scroll_position = $(document).scrollTop();
+
+                console.log(scroll_position)
+
+                if (scroll_position < 50) {
+
+                    $(this.header).addClass(this.cleanClass);
+                } else {
+
+                    $(this.header).removeClass(this.cleanClass);
+                }
+
+            },
+
+            init: function () {
+
+                const _this = this;
+
+                // Check when page is ready
+                this.manageHeader();
+
+                // Check on scroll event
+                $(window).on('scroll', function () {
+
+                    _this.manageHeader();
+                });
             }
-        });
+        }
+
+        variationsManageStickyHeader.init();
 
     });
 })(jQuery);
