3b549e1f301b3eb42b4f41b2d64deb0e5301853e
Maksym Marko <markomaksym@gmail.com>
2024-02-28 12:05:37 +0200

    Added starter pattern.

---
+++ patterns/page-island-homepage.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Island Homepage
+ * Slug: variations/page-island-homepage
+ * Categories: page
+ * Keywords: starter
+ * Block Types: core/post-content
+ * Post Types: page, wp_template
+ * Viewport width: 1140
+ */
+?>
+
+<!-- wp:pattern {"slug":"variations/island-homepage"} /-->
