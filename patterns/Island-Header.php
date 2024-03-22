ed065d532494352812e4c55f8d7bd812f30f7fd2
Maksym Marko <markomaksym@gmail.com>
2024-03-22 12:19:42 +0200

    Version 4.0.2.

--- patterns/Island-Header.php
+++ patterns/Island-Header.php
@@ -9,8 +9,8 @@
  */
 
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"2rem","top":"2rem","left":"1rem","right":"1rem"},"margin":{"top":"0","bottom":"0"},"blockGap":"0"},"position":{"type":"sticky","top":"0px"},"dimensions":{"minHeight":""}},"layout":{"type":"constrained","contentSize":"1140px","wideSize":"1140px"}} -->
-<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:2rem;padding-right:1rem;padding-bottom:2rem;padding-left:1rem"><!-- wp:columns {"isStackedOnMobile":false,"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":{"top":"0px","left":"0px"}}}} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"2rem","top":"2rem","left":"1rem","right":"1rem"},"margin":{"top":"0","bottom":"0"},"blockGap":"0"},"position":{"type":""},"dimensions":{"minHeight":""}},"className":"is-style-header-fixed","layout":{"type":"constrained","contentSize":"1140px","wideSize":"1140px"}} -->
+<div class="wp-block-group is-style-header-fixed" style="margin-top:0;margin-bottom:0;padding-top:2rem;padding-right:1rem;padding-bottom:2rem;padding-left:1rem"><!-- wp:columns {"isStackedOnMobile":false,"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":{"top":"0px","left":"0px"}}}} -->
 <div class="wp-block-columns is-not-stacked-on-mobile" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:column {"verticalAlignment":"center","width":"30%"} -->
 <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:30%"><!-- wp:site-title {"style":{"color":{"text":"#041d55"},"elements":{"link":{"color":{"text":"#041d55"}}},"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"1.2","textTransform":"none","fontSize":"2.01rem"}},"fontFamily":"prata-regular"} /--></div>
 <!-- /wp:column -->
